a1c2f0acbca4dc599151da74d0055c8663989d30
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-08 18:39:26 -0300

    <Banner /> Adicionando SVG tema light

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -4,7 +4,7 @@ function Banner() {
 	return (
 		<header className='bg-backgroundLevel1-light text-textColorBase-light dark:text-textColorBase-dark dark:bg-backgroundLevel1-dark'>
 			<figure className='p-3'>
-				<img src='/aluratube.svg' alt='Logo AluraTube' />
+				<img src='/aluratube-light.svg' alt='Logo AluraTube' />
 			</figure>
 			<figure className='h-72'>
 				<img src='https://source.unsplash.com/random/?Programming' alt='Banner' className='w-full h-full object-cover' />
