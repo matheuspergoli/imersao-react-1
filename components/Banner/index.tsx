5cda639007aa0df9a0f50d7c426aff59f6be908b
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-08 19:17:50 -0300

    <Banner /> Criando renderização condicional de img

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { DarkModeContext } from '../../context/DarkModeContext'
 
 function Banner() {
+	const { darkMode } = React.useContext(DarkModeContext)
+
 	return (
 		<header className='bg-backgroundLevel1-light text-textColorBase-light dark:text-textColorBase-dark dark:bg-backgroundLevel1-dark'>
 			<figure className='p-3'>
-				<img src='/aluratube-light.svg' alt='Logo AluraTube' />
+				{darkMode ? <img src='/aluratube-dark.svg' alt='Logo AluraTube' /> : <img src='/aluratube-light.svg' alt='Logo AluraTube' />}
 			</figure>
 			<figure className='h-72'>
 				<img src='https://source.unsplash.com/random/?Programming' alt='Banner' className='w-full h-full object-cover' />
