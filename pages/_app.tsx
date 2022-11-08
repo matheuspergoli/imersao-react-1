c3b4a8697750ea70cb9a4310054344aff3cc8c9b
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-08 19:15:11 -0300

    Utilizando provider do contexto dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/global.css'
 import type { AppProps } from 'next/app'
+import { DarkModeContextProvider } from '../context/DarkModeContext'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<DarkModeContextProvider>
+			<Component {...pageProps} />
+		</DarkModeContextProvider>
+	)
 }
