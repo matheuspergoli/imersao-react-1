77122756afc3da4b55dcdcc490cb1546ef3676ba
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-08 19:38:01 -0300

    <Banner /> Adicionando botão para mudar tema

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { DarkModeContext } from '../../context/DarkModeContext'
+import ThemeButton from '../ThemeButton'
 
 function Banner() {
 	const { darkMode } = React.useContext(DarkModeContext)
 
 	return (
 		<header className='bg-backgroundLevel1-light text-textColorBase-light dark:text-textColorBase-dark dark:bg-backgroundLevel1-dark'>
-			<figure className='p-3'>
+			<div className='flex items-center justify-between p-3'>
 				{darkMode ? <img src='/aluratube-dark.svg' alt='Logo AluraTube' /> : <img src='/aluratube-light.svg' alt='Logo AluraTube' />}
-			</figure>
+        <ThemeButton />
+			</div>
 			<figure className='h-72'>
 				<img src='https://source.unsplash.com/random/?Programming' alt='Banner' className='w-full h-full object-cover' />
 			</figure>
