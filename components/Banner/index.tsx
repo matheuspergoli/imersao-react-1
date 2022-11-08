46d7176d7c278225d8f4f18c2f4c7602bfc6088c
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-08 20:28:36 -0300

    <Banner /> Adicionando classe responsiva nas imagens

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -9,9 +9,13 @@ function Banner() {
 	return (
 		<header className='bg-backgroundLevel1-light text-textColorBase-light dark:text-textColorBase-dark dark:bg-backgroundLevel1-dark'>
 			<div className='flex items-center justify-between p-3'>
-				{darkMode ? <img src='/aluratube-dark.svg' alt='Logo AluraTube' /> : <img src='/aluratube-light.svg' alt='Logo AluraTube' />}
-        <Search />
-        <ThemeButton />
+				{darkMode ? (
+					<img src='/aluratube-dark.svg' alt='Logo AluraTube' className='hidden sm:block' />
+				) : (
+					<img src='/aluratube-light.svg' alt='Logo AluraTube' className='hidden sm:block' />
+				)}
+				<Search />
+				<ThemeButton />
 			</div>
 			<figure className='h-72'>
 				<img src='https://source.unsplash.com/random/?Programming' alt='Banner' className='w-full h-full object-cover' />
