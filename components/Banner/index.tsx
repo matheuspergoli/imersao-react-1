bc0b0e0a223515e883648be3d5c18d4e26a4d1f7
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-09 15:50:31 -0300

    <Banner /> Importando o novo componente menu

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,22 +1,10 @@
 import React from 'react'
-import { DarkModeContext } from '../../context/DarkModeContext'
-import Search from '../Search'
-import ThemeButton from '../ThemeButton'
+import Menu from '../Menu'
 
 function Banner() {
-	const { darkMode } = React.useContext(DarkModeContext)
-
 	return (
 		<header className='transition bg-backgroundLevel1-light text-textColorBase-light dark:text-textColorBase-dark dark:bg-backgroundLevel1-dark'>
-			<div className='flex items-center justify-between p-3'>
-				{darkMode ? (
-					<img src='/aluratube-dark.svg' alt='Logo AluraTube' className='hidden sm:block' />
-				) : (
-					<img src='/aluratube-light.svg' alt='Logo AluraTube' className='hidden sm:block' />
-				)}
-				<Search />
-				<ThemeButton />
-			</div>
+			<Menu />
 			<figure className='h-72'>
 				<img src='https://source.unsplash.com/random/?Programming' alt='Banner' className='w-full h-full object-cover' />
 			</figure>
