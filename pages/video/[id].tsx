f8a2cfdccc9e23ded7b7f2c0259932d5c3ed7d1d
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-09 19:48:08 -0300

    Adicionando Menu e mudando classes do main

--- pages/video/[id].tsx
+++ pages/video/[id].tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Link from 'next/link'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import Menu from '../../components/Menu'
 import Banner from '../../components/Banner'
 import Profile from '../../components/Profile'
 import { DarkModeContext } from '../../context/DarkModeContext'
-import Link from 'next/link'
 
 function Video() {
 	const { darkMode } = React.useContext(DarkModeContext)
@@ -17,12 +18,13 @@ function Video() {
 				<title>Video</title>
 			</Head>
 			<div className={darkMode ? 'dark' : ''}>
+				<Menu />
 				<Banner />
 				<Profile />
 				<main className='p-3 h-screen transition bg-backgroundBase-light text-textColorBase-light dark:text-textColorBase-dark dark:bg-backgroundBase-dark'>
 					<Link
 						href='/'
-						className='font-medium px-3 py-1 rounded-md bg-backgroundBase-dark text-textColorBase-dark dark:text-textColorBase-light dark:bg-backgroundBase-light'>
+						className='block w-fit font-medium px-3 py-1 mb-5 rounded-md bg-backgroundBase-dark text-textColorBase-dark dark:text-textColorBase-light dark:bg-backgroundBase-light'>
 						Voltar
 					</Link>
 					<iframe src={`https://youtube.com/embed/${id}`} allowFullScreen className='max-w-2xl w-full h-96 mx-auto'></iframe>
