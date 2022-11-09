736da24462539e79ca79a5a923393e74c51f9691
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-08 23:46:12 -0300

    <VideoCard /> Modificando estilos responsivos

--- components/VideoCard/index.tsx
+++ components/VideoCard/index.tsx
@@ -8,7 +8,7 @@ interface VideoProps {
 
 function VideoCard(props: VideoProps) {
 	return (
-		<article className='w-48 overflow-hidden'>
+		<article className='w-48 overflow-hidden md:mx-0 mx-auto'>
 			<a href={props.link} target='_blank' rel='noreferrer'>
 				<img src={props.thumb} alt={props.title} className='transition hover:scale-105' />
 				<h2 className='mt-1 leading-5'>{props.title}</h2>
