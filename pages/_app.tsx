1f8f00b448a50c07ec5ebae2b2e7798733583ea1
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-08 21:08:12 -0300

    Utilizando contexto de busca

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/global.css'
 import type { AppProps } from 'next/app'
 import { DarkModeContextProvider } from '../context/DarkModeContext'
+import { SearchContextProvider } from '../context/SearchContext'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<DarkModeContextProvider>
-			<Component {...pageProps} />
+			<SearchContextProvider>
+				<Component {...pageProps} />
+			</SearchContextProvider>
 		</DarkModeContextProvider>
 	)
 }
