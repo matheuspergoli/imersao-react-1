13f8963721ecbff12e6830e6c078dc0f530c8fb0
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-09 15:54:50 -0300

    <VideoCard /> Modificando Link

--- components/VideoCard/index.tsx
+++ components/VideoCard/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 interface VideoProps {
@@ -9,10 +10,10 @@ interface VideoProps {
 function VideoCard(props: VideoProps) {
 	return (
 		<article className='w-48 overflow-hidden md:mx-0 mx-auto'>
-			<a href={props.link} target='_blank' rel='noreferrer'>
+			<Link href={props.link}>
 				<img src={props.thumb} alt={props.title} className='transition hover:scale-105' />
 				<h2 className='mt-1 leading-5'>{props.title}</h2>
-			</a>
+			</Link>
 		</article>
 	)
 }
