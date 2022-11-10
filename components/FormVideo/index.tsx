c9f92abb375e9c44588aa46e357b14fc84f51bcc
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-10 18:29:44 -0300

    <FormVideo /> Adicionando tema e mudando icones

--- components/FormVideo/index.tsx
+++ components/FormVideo/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Formik, Form, Field } from 'formik'
+import { RiVideoAddFill as AddIcon } from 'react-icons/ri'
 import FormValidation from '../../validation/FormValidation'
-import { AiFillCloseCircle as CloseIcon } from 'react-icons/ai'
+import { IoMdCloseCircle as CloseIcon } from 'react-icons/io'
 
 function FormVideo() {
 	const dialogRef = React.useRef<any>()
@@ -27,7 +28,9 @@ function FormVideo() {
 
 	return (
 		<>
-			<dialog ref={dialogRef} className='transition max-w-sm w-full backdrop:bg-black backdrop:bg-opacity-70 bg-backgroundLevel1-dark'>
+			<dialog
+				ref={dialogRef}
+				className='transition max-w-sm w-full backdrop:bg-opacity-70 backdrop:bg-black bg-backgroundLevel1-light dark:bg-backgroundLevel1-dark'>
 				<Formik validationSchema={FormValidation} onSubmit={(values) => sendVideo(values)} initialValues={{ title: '', url: '' }}>
 					{({ errors, touched }) => (
 						<Form className='flex flex-col items-center justify-center'>
@@ -35,19 +38,33 @@ function FormVideo() {
 								className='flex items-center justify-center rounded-full text-2xl font-semibold absolute top-2 right-4 text-white'
 								type='button'
 								onClick={closeModal}>
-								<CloseIcon />
+								<CloseIcon className='text-black dark:text-white' />
 							</button>
 							<div className='flex flex-col items-center justify-center gap-5 mt-10 mb-5 relative w-full'>
 								<div className='w-full'>
-									<Field id='title' name='title' type='text' placeholder='Título' className='p-1 rounded-md outline-none w-full' />
+									<Field
+										id='title'
+										name='title'
+										type='text'
+										placeholder='Título'
+										className='p-1 rounded-md outline-none w-full text-white bg-backgroundLevel1-dark dark:bg-backgroundLevel1-light dark:text-black'
+									/>
 									{errors.title && touched.title && <p className='text-center text-red-500'>{errors.title}</p>}
 								</div>
 								<div className='w-full'>
-									<Field id='url' name='url' type='text' placeholder='URL' className='p-1 rounded-md outline-none w-full' />
+									<Field
+										id='url'
+										name='url'
+										type='text'
+										placeholder='URL'
+										className='p-1 rounded-md outline-none w-full text-white bg-backgroundLevel1-dark dark:bg-backgroundLevel1-light dark:text-black'
+									/>
 									{errors.url && touched.url && <p className='text-center text-red-500'>{errors.url}</p>}
 								</div>
 							</div>
-							<button className='w-full p-1 rounded-md bg-backgroundLevel1-light' type='submit'>
+							<button
+								className='w-full p-1 rounded-md text-white bg-backgroundLevel1-dark dark:text-black dark:bg-backgroundLevel1-light'
+								type='submit'>
 								Enviar
 							</button>
 						</Form>
@@ -57,7 +74,7 @@ function FormVideo() {
 			<button
 				className='flex items-center justify-center rounded-full fixed bottom-4 right-4 w-14 h-14 z-10 text-xl font-bold bg-red-600'
 				onClick={showModal}>
-				+
+				<AddIcon className='text-white' />
 			</button>
 		</>
 	)
