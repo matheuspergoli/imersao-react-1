ca940f4def7ae45e4c057300c438fcfa2067e029
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-10 14:03:49 -0300

    Adicionando o form

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Slide from '../components/Slide'
 import { GetServerSideProps } from 'next'
 import Banner from '../components/Banner'
 import Profile from '../components/Profile'
+import FormVideo from '../components/FormVideo'
 import { SearchContext } from '../context/SearchContext'
 import { DarkModeContext } from '../context/DarkModeContext'
 
@@ -64,6 +65,7 @@ function Home(props: DataProps) {
 			<Menu />
 			<Banner />
 			<Profile />
+      <FormVideo />
 			<main className='p-3 transition bg-backgroundBase-light text-textColorBase-light dark:text-textColorBase-dark dark:bg-backgroundBase-dark'>
 				<Slide title='Novos' data={filterDataBySearch(props.data.novos)} />
 				<Slide title='Podcasts' data={filterDataBySearch(props.data.podcasts)} />
