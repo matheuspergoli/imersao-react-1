56f9471811a918dcce0f94ac50c34bc394282b5d
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-13 18:41:38 -0300

    <Banner /> Mudando o banner

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -4,7 +4,7 @@ function Banner() {
 	return (
 		<header>
 			<figure className='h-72'>
-				<img src='https://source.unsplash.com/random/?Programming' alt='Banner' className='w-full h-full object-cover' />
+				<img src='/banner.jpg' alt='Banner' className='w-full h-full object-cover' />
 			</figure>
 		</header>
 	)
