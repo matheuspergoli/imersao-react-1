185e685bdbc130a16251c34d2fa6cd180d375278
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-14 04:18:13 -0300

    Mudando rota de revalidate

--- pages/api/revalidate.tsx
+++ pages/api/revalidate.tsx
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 async function handler(req: NextApiRequest, res: NextApiResponse) {
 	let revalidated = false
 	try {
-		await res.revalidate('/')
+		await res.revalidate('https://aluratube-1.vercel.app')
 		revalidated = true
 	} catch (error) {
 		console.log(error)
