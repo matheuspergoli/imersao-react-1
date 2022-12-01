e59eb23e70693f40d2b7ee6fcd5869960d18909f
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-12-01 01:41:39 -0300

    Removendo context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import '../styles/global.css'
 import type { AppProps } from 'next/app'
 import { DarkModeContextProvider } from '../context/DarkModeContext'
-import { SearchContextProvider } from '../context/SearchContext'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<DarkModeContextProvider>
-			<SearchContextProvider>
-				<Component {...pageProps} />
-			</SearchContextProvider>
+			<Component {...pageProps} />
 		</DarkModeContextProvider>
 	)
 }
