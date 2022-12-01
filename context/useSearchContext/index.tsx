fe7bc8a2368ef26d489daaa70c26e5aa8d6ef211
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-12-01 01:40:48 -0300

    <useSearchContext> Criando component para pesquisa

---
+++ context/useSearchContext/index.tsx
@@ -0,0 +1,13 @@
+import create from 'zustand'
+
+interface SearchState {
+	search: string
+	setSearch: (search: string) => void
+}
+
+const useSearchContext = create<SearchState>()((set) => ({
+	search: '',
+	setSearch: (search: string) => set((state) => ({ search }))
+}))
+
+export default useSearchContext
